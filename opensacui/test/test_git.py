0f579920fe91e8b76888cfcf7363f87fd5259f95
natebass <nw3455@gmail.com>
2024-12-29 21:14:19 -0800

    ### QUICK COMMIT ###

--- opensacui/test/test_git.py
+++ opensacui/test/test_git.py
@@ -2,6 +2,7 @@ from opensacui import git
 
 
 def test_clone_project():
+    a = 3
     git.CloneProject(
         "https://github.com/natebass/opensac-ui",
         "/home/nwb/Source/Repos/opensac-ui-docs/_build/natebass-opensac-ui",
